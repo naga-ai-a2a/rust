06b1f21311ca16168d523da49ee0366d6bb219c8
Naga Vijayapuram <nvijayap@gmail.com>
2026-05-26 18:19:49 -0700

    Cleanup

--- src/ret-boxed-err.rs
+++ src/ret-boxed-err.rs
@@ -9,15 +9,6 @@ fn manual_error() -> Result<(), Box<dyn Error>> {
     return Err("something went wrong".into()); 
 }
 
-// #[derive(Debug)]
-// struct MyError;
-// impl std::fmt::Display for MyError { Err("An error has occurred") }
-// impl std::error::Error for MyError {}
-
-// fn custom_box() -> Result<(), Box<dyn Error>> {
-//     Err(Box::new(MyError))
-// }
-
 fn main() {
     println!("\n\t{:?}", do_something());
     println!("\t{:?}", manual_error());
