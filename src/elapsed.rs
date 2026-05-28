00505a2f7696f7c7bca6199f4c769f08e0562f5e
Naga Vijayapuram <nvijayap@gmail.com>
2026-05-27 19:07:09 -0700

    Improved

--- src/elapsed.rs
+++ src/elapsed.rs
@@ -1,15 +1,13 @@
-use std::time::{Duration, SystemTime};
-use std::thread::sleep;
-
 fn main() {
-   let now = SystemTime::now();
+   let now = std::time::SystemTime::now();
 
    // we sleep for 2 seconds
-   sleep(Duration::new(2, 0));
+   std::thread::sleep(std::time::Duration::new(2, 0));
+
    match now.elapsed() {
        Ok(elapsed) => {
            // it prints '2'
-           println!("{}", elapsed.as_secs());
+           println!("\n\tElapsed time: {} seconds", elapsed.as_secs());
        }
        Err(e) => {
            // the system clock went backwards!
